40ee0d3418c932ac764d32f1f9f3c317a742b660
Me <lenulja87@mail.ru>
2020-04-02 16:03:11 +0300

    Это первый коммит

---
+++ project/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*let arr = ['plum.png', 'orange.jpg'];
+
+console.log(arr[0]);*/
+
+// alert("hello");
+
+// let answer = confirm("Are you here?")
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + "- object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr++);
+
+console.log(5%2);
